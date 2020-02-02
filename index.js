515d5ed726f189435ec7839ce57983146e3ca15f
Anar Chalkhaasuren <annachak9@gmail.com>
2020-02-02 17:14:57 -0500

    removed query parameters from cocktail api

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict'
-
-const searchUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php'
+const searchTerm = ''
+const searchUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const newsApiKey = "4c7b0c19e9d74afe982d773604074e2f"
-const newsUrl = "https://newsapi.org/v2/everything?q=cocktails&language=en";
+const newsUrl = 'https://newsapi.org/v2/everything?apiKey=4c7b0c19e9d74afe982d773604074e2f&q=cocktails&pageSize=5';
 const videosUrl = 'https://www.googleapis.com/youtube/v3/videos'
-const videoApiKey = "AIzaSyC1arTFhv6taqAGkrGe-T7X4DlbtFitBP8"
+const videoApiKey = "AIzaSyBMYHHhHGGo-bjKN9WUx1O1jMrTuDnrMkw"
 
+const allUrls = [
+
+];
 
 //function to get news related to cocktails
 function newsSection() {
@@ -14,6 +17,7 @@ function newsSection() {
             "X-Api-Key": newsApiKey
         })
     };
+
     fetch(newsUrl, options)
         .then(response => response.json())
         .then(responseJson => console.log(responseJson));
@@ -27,30 +31,26 @@ function formatQueryParams(params) {
     return queryItems.join('&');
 }
 
-// function displayResults(responseJson) {
-//     // if there are previous results, remove them
-//     // console.log(responseJson);
-//     $('#results-list').empty();
-//     // iterate through the items array
-//     for (let i = 0; i < responseJson.items.length; i++) {
-//         // for each video object in the items 
-//         //array, add a list item to the results 
-//         //list with the video title, description,
-//         //and thumbnail
-//         $('#results-list').append(
-//             `<li><h3>${responseJson.items[i].snippet.title}</h3>
-//       <p>${responseJson.items[i].snippet.description}</p>
-//       <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
-//       </li>`
-//         )
-//     };
-//     //display the results section  
-//     $('#results').removeClass('hidden');
-//  };
+
+//function to get the recipe
+function getRecipe() {
+const recipeSearch = searchUrl + searchTerm
+    fetch(recipeSearch)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+};
 
 
 //function to get youtube videos 
-function getYoutubeVideos(argument) {
+function getVideos(argument) {
     const params = {
         key: videoApiKey,
         part: 'snippet',
@@ -61,32 +61,18 @@ function getYoutubeVideos(argument) {
     console.log(urlYoutube);
 
     fetch(urlYoutube)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson));
 };
 
-
-
-//     fetch(searchUrl)
-
-//         .then(response => {
-//             if (response.ok) {
-//                 return response.json();
-//             }
-//             //throw new Error(response.statusText);
-//         })
-//         // .then(responseJson => displayResults(responseJson))
-//         .catch(err => {
-//             $('#js-error-message').text(`Something went wrong: ${err.message}`);
-//         });
-// }
-
+//event listener 
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
-        const searchTerm = $('#js-search-term').val();
-        getYoutubeVideos(searchTerm);
-        newsSection("testing");
+        this.searchTerm = $('#js-search-term').val();
+        //getVideos(searchTerm);
+        newsSection();
+        getRecipe();
     });
 }
 

7aa9c870ac2682ce2bb51e63bf9781df87d8705c
Anar Chalkhaasuren <annachak9@gmail.com>
2020-02-03 20:10:41 -0500

    get recipe fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const searchTerm = ''
+//const searchTerm = ''
 const searchUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const newsApiKey = "4c7b0c19e9d74afe982d773604074e2f"
 const newsUrl = 'https://newsapi.org/v2/everything?apiKey=4c7b0c19e9d74afe982d773604074e2f&q=cocktails&pageSize=5';
@@ -34,24 +34,44 @@ function formatQueryParams(params) {
 
 //function to display videos and recipes
 function displayResults(responseJson) {
-
+    responseJson.drinks.length
     $('#results').empty();
     // const preparedData = responseJson.drinks[0].
-    for (let i = 0; i < responseJson.drinks.length; i += 5) {
+    console.log(responseJson)
+    for (let i = 0; i < 5; i ++) {
 
-        //prepare ingredients
-        const ingKeys = [];
+        // //prepare ingredients
+        // const ingKeys = [];
 
-        //preprare measure
-        const measureKeys = [];
+        // //preprare measure
+        // const measureKeys = [];
 
-        //get all the object properties
-        const objKeys = Object.keys(responseJson.drinks[i]);
-        for (let j = 0; j < objKeys.length; j++) {
-            if (objKeys[j].substring(0, 14) === 'strIngredient') {
-                ingKeys.push(responseJson.drinks[i]['strIngredient' + (j + 1)])
-            }
-        } 
+        // //get all the object properties
+        // const objKeys = Object.keys(responseJson.drinks[i]);
+        // for (let j = 0; j < objKeys.length; j++) {
+        //     if (objKeys[j].substring(0, 14) === 'strIngredient') {
+        //         ingKeys.push(responseJson.drinks[i]['strIngredient' + (j + 1)])
+        //     }
+           
+        // } 
+
+         //prepare ingredients
+               const ingredients = [];
+
+               //preprare measure
+               const measureKeys = [];
+       
+               //get all the object properties
+               const objKeys = Object.keys(responseJson.drinks[i]);
+        
+               for (let j = 0; j < objKeys.length; j++) {
+                  
+                   if (objKeys[j].includes('strIngredient') && responseJson.drinks[i][objKeys[j]] != null) {
+                       ingredients.push(responseJson.drinks[i][objKeys[j]])
+                   }
+               } 
+             
+               console.log(ingredients);
 
 
 
@@ -61,7 +81,7 @@ function displayResults(responseJson) {
                     <li><h3>Cocktail Name: </h3><p>${responseJson.drinks[i].strDrink}</p></li>
                     <li><h3>Instructions: </h3><p>${responseJson.drinks[i].strInstructions}</p></li>
                     <li>Thumbnail: <img src="${responseJson.drinks[i].strDrinkThumb}" height="100" width="100" ></img></li>
-                    <li><h4>Ingredients: </h3><p>${responseJson.drinks[i].strIngredient}</p></li>
+                    <li><h4>Ingredients: </h3><p>${ingredients}</p></li>
                     <li><h4>Measure: </h3><p>${responseJson.drinks[i].strMeasure1}</p></li>
                     </ul>
             </div>`
@@ -75,8 +95,9 @@ function displayResults(responseJson) {
 
 
 //function to get the recipe
-function getRecipe() {
+function getRecipe(searchTerm) {
     const recipeSearch = searchUrl + searchTerm
+    console.log(recipeSearch)
     fetch(recipeSearch)
         .then(response => {
             if (response.ok) {
@@ -88,6 +109,7 @@ function getRecipe() {
         .catch(err => {
             $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
+     
 };
 
 
@@ -105,7 +127,7 @@ function getVideos(argument) {
 
     fetch(urlYoutube)
         .then(response => response.json())
-        .then(responseJson => console.log('testing', responseJson));
+        .then(responseJson => console.log(responseJson));
 };
 
 
@@ -115,9 +137,9 @@ function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
         this.searchTerm = $('#js-search-term').val();
-        getVideos(searchTerm);
+        getVideos(this.searchTerm);
         newsSection();
-        getRecipe();
+        getRecipe(this.searchTerm);
     });
 }
 
